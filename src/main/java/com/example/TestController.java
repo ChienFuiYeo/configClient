679c6e7522ef77b03510cb5ac505acc0176a9b29
yeo <yeo880726@hotmail.com>
2017-04-09 16:54:04 +0800

    init

---
+++ src/main/java/com/example/TestController.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+	
+	@Value("${test.home:Default}")
+	private String testHomeWelcome;
+	
+	@Value("${test.message:Default}")
+	private String testHomeMessage;
+	
+	@Value("${global.message}")
+	private String testGlobalMessage;
+	
+	@RequestMapping("/testHome")
+	public String testHomeWelcome(){
+		return testHomeWelcome;
+	}
+	
+	@RequestMapping("/testHomeMessage")
+	public String testHomeMessage(){
+		return testHomeMessage;
+	}
+	
+	@RequestMapping("/testGlobalMessage")
+	public String testGlobalMessage(){
+		return testGlobalMessage;
+	}
+}
